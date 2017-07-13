d87b2ffb28e4928836ca9dc0d14551d2901f8bb0
Yogesh Fadtale <yog989@gmail.com>
2017-07-13 21:46:37 +0530

    validation and output formatting

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 require('vendor/autoload.php');
 
-//$obj= new \App\MainEntry('C2', 'PAWN');
-$obj= new \App\MainEntry('D4', 'KING');
+use Symfony\Component\HttpFoundation\Request;
+
+$validPieces = ['KING' , 'ROOK', 'BISHOP', 'QUEEN', 'HORSE', 'PAWN'];
+$validPositions = "/^[A-H|a-h][1-8]$$/";
+
+if (defined('STDIN')) {
+    if ($argc < 3) {
+        die("Usage : index.php <type> <position>");
+    }
+    $type = $argv[1];
+    $position = $argv[2];
+} else {
+    $request = Request::createFromGlobals();
+    $type = $request->query->get('type', '');
+    $position = $request->query->get('pos', '');
+}
+
+if (!preg_match($validPositions, $position)) {
+    die("Invalid Position");
+}
+
+if (!in_array(strtoupper($type), $validPieces, true)) {
+    die("Invalid Piece");
+}
+
+$selectedPiece = \App\TypeFactory::getType(strtoupper($type));
+$possibleMoves = $selectedPiece->move(new \App\ChessBoard(), strtoupper($position));
+echo implode(", ", $possibleMoves);
